c2c2b3f6b2b9439b6820e63d80cfddad5f3604c8
SergeevFun <sergeev.nlint@yandex.ru>
2021-05-22 13:25:20 +0300

    Созданы шаблоны, реализована отправка данных

--- handlers.go
+++ handlers.go
@@ -3,17 +3,105 @@ package main
 import (
 	"net/http"
 	"html/template"
-
+	// "fmt"
 	// "encoding/json"
 	// "time"
 )
 
+type slider struct {
+	Img string
+	Title string
+	Subtitle string
+	Description string
+	Btn string
+}
+
+type match struct {
+	Id string
+	Name string
+	Total string
+}
+
+type sorevnovanie struct {
+	Id string
+	Name string
+	Icon string
+	Match match 
+}
+
+type hockey struct {
+	
+}
+
+type volleyball struct {
+	
+}
+
+type tabletennis struct {
+	
+}
+
+type feildhockey struct {
+	Header string
+	Href string
+	Sorevnovanie sorevnovanie
+}
+
+type basketball struct {
+	
+}
+
+type football struct {
+	
+}
+
+type content struct {
+	Hockey hockey
+	Volleyball volleyball
+	TableTennis tabletennis
+	FieldHockey feildhockey
+	Basketball basketball
+	Football football
+}
+
+type data struct {
+	Slider slider
+	Content content
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	
+	send := data {
+		Slider: slider {
+			Img: "img/slider-1-slide-1-1920x670.jpg",
+			Title: "Самый первый",
+			Subtitle: "Подзаголовок",
+			Description: "Основной текст",
+			Btn: "Текст на кнопке",
+		},
+		Content: content {
+			FieldHockey: feildhockey {
+				Header: "Хоккейна траве",
+				Href: "/field_hockey/",
+				Sorevnovanie: sorevnovanie {
+					Id: "3",
+					Name: "Кубок России",
+					Icon: "/img/sport-icons/field-hockey.svg",
+					Match: match {
+						Id: "1",
+						Name: "Зеленодольск - Казань",
+						Total: "1 - 0",
+					},
+				},
+			},
+		},
+	}
+
 	files := []string {
 		"templates/pages/main.page.tmpl",
 		"templates/layouts/index.layout.tmpl",
+		"templates/partials/new.partial.tmpl",
 	}
 	tmpl, _ := template.ParseFiles(files...)
-	tmpl.Execute(w, nil)
+	tmpl.Execute(w, send)
 }
